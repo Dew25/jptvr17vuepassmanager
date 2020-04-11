62445fdc24555bc4207d7e77baf7ca775c6a54dd
JVM <dew2@yandex.ru>
2020-04-11 21:08:39 +0300

    Создан проект + меню бар на главной странице

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "publicPath": "/JPTVR17PassManager",
+  "outputDir": "C:\\Users\\Irina\\Documents\\NetBeansProjects\\JPTVR17PassManager\\web\\vue",
+  "runtimeCompiler": true,
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
